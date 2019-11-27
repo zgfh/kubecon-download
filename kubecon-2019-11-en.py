e4a99710719093a637248f280e2e5b301df25582
alan <zg.zhu@daocloud.io>
2019-11-27 23:39:27 +0800

    add kube conf 2019-11

---
+++ kubecon-2019-11-en.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+@version: 1.0.0
+@author: zheng guang
+@contact: zg.zhu@daocloud.io
+@time: 2019/6/26 5:49 PM
+"""
+
+import kubecon
+
+if __name__ == '__main__':
+    kubecon.Main('events', [
+        "https://kccncna19.sched.com/?iframe=no"])
+    pass
